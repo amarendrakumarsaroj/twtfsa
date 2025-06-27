bd0e9a2a1243293a6aa3b0230c292d739470c79e
Amarendra Kumar Saroj <amarendra@nrb.org.np>
2025-06-27 12:06:50 +0545

    created create_contacts() with graceful api try catch blocks check

--- backend/main.py
+++ backend/main.py
@@ -10,7 +10,22 @@ def get_contacts(): #Valid any function name
                                                          #  x variable ma contact list aucha, x.to_json() function चल्छ then contacts bhanne naya list ma data pathaidincha
     return jsonify({"contacts": json_contacts})
 
-
+@app.route("/contacts", methods=["POST"])
+def create_contacts():
+    first_name = request.json.get("firstName"),
+    last_name = request.json.get("lastName"),
+    email = request.json.get("email")
+    if not first_name or not last_name or not email:
+        return (
+            jsonify({"message": "You must include a first name , last name or email"}), 400,
+        )
+    new_contact = Contact(first_name=first_name, last_name=last_name, email=email)   
+    try:
+        db.session.add(new_contact)
+        db.session.commit()
+    except Exception as e:
+        return jsonify({"message": str(e)}), 400
+    return jsonify({"message": "User Created!"}), 201 
 
 if __name__=="__main__":
     #create all tables
