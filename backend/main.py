bfbaa31d06c4e099dcba8c48cbf1de82b0f25406
Amarendra Kumar Saroj <amarendra@nrb.org.np>
2025-06-27 11:39:16 +0545

    JSON serialization not possible for map(...) function so added list(map(....)) for the issue resolution

--- backend/main.py
+++ backend/main.py
@@ -6,11 +6,9 @@ from models import Contact
 @app.route("/contacts", methods=["GET"])
 def get_contacts(): #Valid any function name
     contacts = Contact.query.all() # query.all but logically you can filter when specific data only
-    json_contacts = map(lambda x: x.to_json(), contacts) # lambda function भनेको shortcut तरिकाले function लेख्ने हो :
+    json_contacts = list(map(lambda x: x.to_json(), contacts)) # lambda function भनेको shortcut तरिकाले function लेख्ने हो :
                                                          #  x variable ma contact list aucha, x.to_json() function चल्छ then contacts bhanne naya list ma data pathaidincha
     return jsonify({"contacts": json_contacts})
-
-
 
 
 
--- backend/models.py
+++ backend/models.py
@@ -5,7 +5,7 @@ class Contact(db.Model):
     __tablename__ = 'contacts'
     #explicitly define the tablename
     
-    id = db.Column(db.Interger, primary_ke=True)
+    id = db.Column(db.Integer, primary_key=True)
     first_name = db.Column(db.String(80), unique=False, nullable=False)
     last_name = db.Column(db.String(80), unique=False, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
