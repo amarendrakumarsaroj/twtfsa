baf2594c1e6b23a4bad50d0dc4525de572969f8b
Amarendra Kumar <dominonepal@gmail.com>
2025-06-28 07:17:21 +0545

    added codes of main.py

--- backend/main.py
+++ backend/main.py
@@ -12,8 +12,8 @@ def get_contacts(): #Valid any function name
 
 @app.route("/contacts", methods=["POST"])
 def create_contacts():
-    first_name = request.json.get("firstName"),
-    last_name = request.json.get("lastName"),
+    first_name = request.json.get("firstName")
+    last_name = request.json.get("lastName")
     email = request.json.get("email")
     if not first_name or not last_name or not email:
         return (
@@ -24,8 +24,36 @@ def create_contacts():
         db.session.add(new_contact)
         db.session.commit()
     except Exception as e:
-        return jsonify({"message": str(e)}), 400
+        return jsonify({"error": str(e)}), 400
     return jsonify({"message": "User Created!"}), 201 
+
+ # Update a contact
+@app.route("/update_contact/<int:user_id>", methods=["PATCH"])
+def update_contact(user_id):
+    contact_to_update = Contact.query.get(user_id)
+    
+    if not contact_to_update:
+        return jsonify({"message": "User Not Found"}), 404
+    data = request.json
+    contact_to_update.first_name = data.get("firstName", contact_to_update.first_name)
+    contact_to_update.last_name = data.get("lastName", contact_to_update.last_name)
+    contact_to_update.email = data.get("email", contact_to_update.email)
+    db.session.commit()
+    return jsonify({
+        "message": "User Updated"
+    }), 200
+
+@app.route("/delete_contact/<int:user_id>", methods=["DELETE"])
+def delete_user(user_id):
+    contact_to_delete = Contact.query.get(user_id)
+    if not contact_to_delete:
+        return jsonify({
+            "error":"User Not Found"
+        }), 404
+    db.session.delete(contact_to_delete)
+    db.session.commit()
+    return jsonify({"message":"User Deleted!"}),200    
+        
 
 if __name__=="__main__":
     #create all tables
