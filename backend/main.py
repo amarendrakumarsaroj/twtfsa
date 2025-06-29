9d163f8237bd85d51da978243934c9acff696339
Amarendra Kumar Saroj <amarendra@nrb.org.np>
2025-06-29 10:12:18 +0545

    all api endpoints of contacts worked well

--- backend/main.py
+++ backend/main.py
@@ -12,12 +12,12 @@ def get_contacts(): #Valid any function name
 
 @app.route("/contacts", methods=["POST"])
 def create_contacts():
-    first_name = request.json.get("firstName"),
-    last_name = request.json.get("lastName"),
+    first_name = request.json.get("firstName")
+    last_name = request.json.get("lastName")
     email = request.json.get("email")
     if not first_name or not last_name or not email:
         return (
-            jsonify({"message": "You must include a first name , last name or email"}), 400,
+            jsonify({"error": "You must include a first name , last name and email. All three of them."}), 400,
         )
     new_contact = Contact(first_name=first_name, last_name=last_name, email=email)   
     try:
@@ -26,6 +26,35 @@ def create_contacts():
     except Exception as e:
         return jsonify({"message": str(e)}), 400
     return jsonify({"message": "User Created!"}), 201 
+
+#update the contact
+@app.route("/update_contacts/<int:user_id>", methods=["PATCH"])
+def update_contact(user_id):
+    contact = Contact.query.get(user_id)
+    if not contact:
+        return jsonify({"message": "User not found!"}), 404
+    
+    data = request.json
+    contact.first_name = data.get("firstName", contact.first_name) 
+    contact.last_name = data.get("lastName", contact.last_name) 
+    contact.email = data.get("email", contact.email)
+    
+    db.session.commit()
+    return jsonify({"message": "User Updated!"}), 200 
+     
+#delete the contact
+@app.route("/delete_contacts/<int:id>", methods=["DELETE"])
+def delete_contact(id):
+    contact = Contact.query.get(id)
+    if not contact:
+        return jsonify({"message": "User not found!"}), 404
+    db.session.delete(contact)    
+    db.session.commit()
+    return jsonify({"message": "User Deleted!"}), 200 
+        
+        
+
+
 
 if __name__=="__main__":
     #create all tables
